4cd0d2819a0683f343e4b7baf805d254f9f9fb6b
Esteban Sánchez <esteban.sanvaz@gmail.com>
2024-01-16 12:18:26 -0600

    comment client trpc shit

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,12 @@
-import { Client } from "@/components/client";
-import { Server } from "@/components/server";
-
+// import { Client } from "@/components/client";
+import { Server } from '@/components/server';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>Hola Mundo!</h1>
 
       <Server />
     </main>
-  )
+  );
 }
